3f2b24b4a0bcd9116a09953773cfe832b20cf52d
김영륜 <parkqwre@gmail.com>
2025-09-03 15:49:23 +0900

    Updates

---
+++ _next/static/chunks/41456f0ff92d879b.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/815b51cc09e6dd8c.js",
+  "static/chunks/turbopack-e92a45289dc2dc78.js"
+])
--- _next/static/x_d-A4cJ2Xw1wjzwFNukI/_buildManifest.js
+++ _next/static/x_d-A4cJ2Xw1wjzwFNukI/_buildManifest.js
@@ -1,6 +1,6 @@
 self.__BUILD_MANIFEST = {
   "/_error": [
-    "./static/chunks/22f805491aa2a4cf.js"
+    "./static/chunks/6cd60197269122f1.js"
   ],
   "__rewrites": {
     "afterFiles": [],
