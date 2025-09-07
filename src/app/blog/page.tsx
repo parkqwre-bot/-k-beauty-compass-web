6fc2739c4e635e86aa7c9034080860e1abba021a
김영륜 <parkqwre@gmail.com>
2025-09-07 22:08:45 +0900

    feat: Add blog list page skeleton

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,8 @@
+export default function BlogPage() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-4xl font-bold">Blog</h1>
+      <p>Welcome to the blog. Posts will be listed here soon.</p>
+    </div>
+  );
+}
