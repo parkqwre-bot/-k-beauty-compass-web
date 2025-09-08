a22054a197b0efe97bb81c25709a2f05f8dcf666
김영륜 <parkqwre@gmail.com>
2025-09-08 17:51:44 +0900

    fix: Include content directory in standalone build

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
 /** @type {import('next').NextConfig} */
+const CopyPlugin = require('copy-webpack-plugin');
+
 const nextConfig = {
-  i18n: {
-    locales: ['en', 'ko'],
-    defaultLocale: 'en',
+  output: 'standalone',
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins.push(
+        new CopyPlugin({
+          patterns: [
+            {
+              from: './content',
+              to: './content',
+            },
+          ],
+        })
+      );
+    }
+    return config;
   },
 };
 
