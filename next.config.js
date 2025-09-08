1bb2c3ecc0b0ce02aef7a970b4e5042f56245687
김영륜 <parkqwre@gmail.com>
2025-09-08 17:58:00 +0900

    revert: Last next.config.js changes causing 404

--- next.config.js
+++ next.config.js
@@ -1,22 +1,8 @@
 /** @type {import('next').NextConfig} */
-const CopyPlugin = require('copy-webpack-plugin');
-
 const nextConfig = {
-  output: 'standalone',
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins.push(
-        new CopyPlugin({
-          patterns: [
-            {
-              from: './content',
-              to: './content',
-            },
-          ],
-        })
-      );
-    }
-    return config;
+  i18n: {
+    locales: ['en', 'ko'],
+    defaultLocale: 'en',
   },
 };
 
