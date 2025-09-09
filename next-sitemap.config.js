89da8ca0cde2e5e95d8c58d12d193ab45c505992
김영륜 <parkqwre@gmail.com>
2025-09-10 01:34:31 +0900

    feat: 사이트맵 자동 생성 기능 추가

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://k-beauty-compass-web.vercel.app',
+  generateRobotsTxt: true, // (Optional)
+  // ...other options
+}
